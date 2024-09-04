ce3f532d0a5793e34105c0d2f21825ad9442e887
keithmmc <100876230+keithmmc@users.noreply.github.com>
2024-09-04 22:10:41 +0100

    Update pro.go

--- pro.go
+++ pro.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"encoding/csv"
 	"fmt"
 	"os"
@@ -19,7 +20,7 @@ type ProductStock struct {
 	Quantity int
 }
 
-type shop struct {
+type Shop struct {
 	Cash  float64
 	Stock []ProductStock
 }
@@ -67,4 +68,61 @@ func createAndStockShop() Shop {
 		s.Stock = append(s.Stock, ps)
 	}
 	return s
+}
+
+func readCustomer() Customer {
+	var c Customer
+	reader := bufio.NewReader(os.Stdin)
+	fmt.print("please upload your customer file name here.... ")
+	path, _ := reader.ReadString('\n')
+	path = "../" + strings.TrimSpace(path) + ".csv"
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println("incorrect customer file name,")
+		returnToMenu()
+		return c
+
+	}
+	defer file.Close()
+	csvReader := csv.NewReader(file)
+	firstRow, _ := csvReader.Read()
+	c.Name = firstRow[0]
+	c.Budget, _ = strconv.ParseFloat(firstRow[1], 64)
+	for {
+		row, err := csvReader.Read()
+		if err != nil {
+			break
+		}
+
+		quantity, _ := strconv.Atoi(row[1])
+		p := Product{Name: row[0]}
+		ps := ProductStock{Product: p, Quantity: quantity}
+		c.ShoppingList = append(c.ShoppingList, ps)
+	}
+
+	return c
+
+}
+func printProduct(p Product) {
+	fmt.Printf("\nPRODUCT NAME: %s \nPRODUCT PRICE: €%.2f\n", p.Name, p.Price)
+}
+
+func printShop(s Shop) {
+	fmt.Printf("Shop has €%.2f in cash\n", s.Cash)
+	for _, item := range s.Stock {
+		printProduct(item.Product)
+		fmt.Printf("The Shop has %d of the above\n", item.Quantity)
+		fmt.Println("-------------")
+	}
+}
+
+func returnToMenu() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("press any key to return to main menu")
+	reader.ReadString('\n')
+	showMenu()
+}
+
+func showMenu() {
+
 }
